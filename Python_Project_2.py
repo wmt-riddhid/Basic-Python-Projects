55e09cf1c9a81c28f5d6b8c855cb47c388ea6a12
riddhid <riddhid@webmob.tech>
2022-02-11 17:02:36 +0530

    Guessing the number game (user)

---
+++ Python_Project_2.py
@@ -0,0 +1,21 @@
+# This is a guess the number game (user)
+
+import random
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ""
+    while feedback != "c" :
+        if low != high :
+            guess = random.randint(low, high)
+        else :
+            guess = low
+        feedback = input("It's " + str(guess) + " is too low (L), It is too high (H), It is correct (C) ? ").lower()
+        if feedback == "h" :
+            high = guess - 1
+        elif feedback == "l" :
+            low = guess + 1
+    print("Yayyyyyy !!!!! Congrulations...the computer guessed your number , " + str(guess) + " , correctly")
+
+computer_guess(10)
