17956ec4430059713eabe256c6f1391c56e1992b
riddhid <riddhid@webmob.tech>
2022-02-15 11:45:03 +0530

    countdown timmer, python project

---
+++ countdown.py
@@ -0,0 +1,12 @@
+import time
+
+def countdown(t):
+    while t:
+        minutes, seconds = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(minutes, seconds)
+        print(timer, end = "\r")
+        time.sleep(0.5)
+        t -= 1
+    print("Timer completed !")
+t = input("Please enter the time in seconds: ")
+countdown(int((t)))
